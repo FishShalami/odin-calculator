3e6358f7f15111366369eb36f63a4a89b75e7923
FishShalami <fishshalami@gmail.com>
2024-11-15 22:15:02 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//functions for add, subtract, multiply, divide
+
+//create three variables for num1, operator, num2
+
+//Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
+
