133593facd99a7040b7b6503ff999f5506ae85cd
FishShalami <fishshalami@gmail.com>
2024-11-16 22:56:49 -0800

    Bttn color, listeners for num bttn and operator, css flex adjust

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//functions for changing buttons on click
+const buttons = document.querySelectorAll('button')
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(e) {
+        this.classList.add("clicked");
+        setTimeout(() => {
+            this.classList.remove("clicked");
+        }, 150);
+    });
+
+}
+
+
+
 //functions for add, subtract, multiply, divide
 
 const add = function(a, b) {
@@ -36,9 +51,64 @@ const operate = function(a, b, operator) {
 //functions for display of numbers
 
 
+const numSelect = document.querySelectorAll(".num");
+let numSelectValue = ''
+let num1 = ''
+let num2 = ''
 
-//create three variables for num1, operator, num2
+const userNumSelect = function() {
+    for(let i = 0; i < numSelect.length; i++) {
+        numSelect[i].addEventListener("click", function(e) {
+            numSelectValue += Number(e.target.innerText); //allows repeated button clicks for numbers like 222, or 1133
+            // console.log("You clicked the " + numSelectValue + " button");
+            displayNumber.textContent = numSelectValue;
+            // num1 = numSelectValue;
+            return numSelectValue;
+        });
+    
+    }
+}
 
-let num1 = 1;
-let num2 = 1;
-let operator = '+';
+
+//operator selection function:
+const operatorSelect = document.querySelectorAll('.operator');
+let opSelectChar = ''
+
+const userOperatorSelect = function() {
+    for(let i = 0; i < operatorSelect.length; i++) {
+        operatorSelect[i].addEventListener("click", function(e) {
+            opSelectChar = e.target.innerText; 
+            console.log("You clicked the " + opSelectChar + " button");
+            return opSelectChar;
+        });
+    }
+}
+
+userNumSelect();
+userOperatorSelect();
+
+//display the value in the 'screen' of the calculator
+const displayScreen = document.querySelector(".display-num");
+let displayNumber = document.createElement('p');
+displayScreen.appendChild(displayNumber);
+
+//initialize display with the initial value
+displayNumber.textContent = numSelectValue;
+
+
+
+//if user selects clear, then set all variables to default. clearing function:
+//variables set to '' or 0 and p element for the display set to ''
+
+
+
+//logic for operations:
+    //clear screen --> if user selects numbers, store number in a variable, num1
+    //once user selects an operator, store the operator selected as a variable, operator
+    //the next number entered is to be used as other number variable, num2
+    // use the operator function for operate(num1, num2, operator)
+    //store the result from operate in the calc display window p element
+
+
+
+
